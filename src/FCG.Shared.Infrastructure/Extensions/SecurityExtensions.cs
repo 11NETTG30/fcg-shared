f80f38c5fe64eae94133f91cfa7559f856d638b1
gaabrielalex <gabrielalexsilva2@gmail.com>
2026-05-25 21:03:32 -0400

    feat(security): implementar extensões de segurança padronizadas (AddFCGSecurity)

---
+++ src/FCG.Shared.Infrastructure/Extensions/SecurityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FCG.Shared.Infrastructure.Configurations;
+using Microsoft.AspNetCore.Builder;
+
+namespace FCG.Shared.Infrastructure.Extensions
+{
+	public static class SecurityExtensions
+	{
+		public static WebApplicationBuilder AddFCGSecurity(this WebApplicationBuilder builder)
+		{
+			builder.Services.AddIdentityPropagation();
+
+			return builder;
+		}
+	}
+}
